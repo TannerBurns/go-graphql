603eed869aed0e9853a8eabee205532cef3ebf21
Tanner Burns <tjburns102@gmail.com>
2019-02-11 22:24:34 -0600

    updated to get method with query in params instead of request body

--- controllers/users.go
+++ controllers/users.go
@@ -14,25 +14,20 @@ type reqBody struct {
 func (c *Controller) Users(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
+	query := r.URL.Query().Get("query")
 	// Check to ensure query was provided in the request body
-	if r.Body == nil {
-		http.Error(w, "Must provide graphql query in request body", 400)
+	if query == "" {
+		c.Logger.Logging(r, 400)
+		http.Error(w, "Must provide graphql query in params", 400)
 		return
 	}
 
-	var rBody reqBody
-	// Decode the request body into rBody
-	err := json.NewDecoder(r.Body).Decode(&rBody)
-	if err != nil {
-		http.Error(w, "Error parsing JSON request body", 400)
-	}
-
 	// Execute graphql query
-	result := models.ExecuteQuery(rBody.Query, *c.GqlSchema)
+	result := models.ExecuteQuery(query, *c.GqlSchema)
 
 	// marshalling to json,
 	// the Content-Type as application/json.
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	c.Logger.Logging(r, 200)
 	json.NewEncoder(w).Encode(result)
